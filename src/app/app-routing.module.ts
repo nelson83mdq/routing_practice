6fa5f4cc839ee10a88afecbf23f6e9665c9a434d
Nelson Damian Brizuela <nelson83mdq@gmail.com>
2024-04-19 20:45:36 -0300

    using app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HowComponent } from './how/how.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes=[
+  {path: '', component: HomeComponent},
+  {path: 'how', component: HowComponent},
+  {path: 'productos', component: ProductsComponent},
+  {path: 'contacto', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HowComponent } from './how/how.component';
-import { ContactComponent } from './contact/contact.component';
-import { ProductsComponent } from './products/products.component';
-import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
-const routes: Routes=[
-  {path: '', component: HomeComponent},
-  {path: 'how', component: HowComponent},
-  {path: 'productos', component: ProductsComponent},
-  {path: 'contacto', component: ContactComponent}
-];
 
 
 @NgModule({
   declarations: [				
     AppComponent,
-      HomeComponent,
-      HowComponent,
-      ContactComponent,
-      ProductsComponent
+      
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
